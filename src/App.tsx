ba1602597feefa3d78e696fb9d861f3c4fc83caf
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2024-11-02 17:25:17 +0900

    整理。

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,11 @@ import './App.css';
 
 import React from 'react';
 
-import logo from './logo.svg';
-
 export default function App(): React.JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>React Bootstrap Modal sample</h1>
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,21 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+
+import { Router } from '@remix-run/router';
 
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+const router: Router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />} />
+  )
 );
+
+const root: ReactDOM.Root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
